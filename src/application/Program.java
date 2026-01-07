ef0a62ab5bdf2428d726847e7d17aad5a42b439c
pierry-savio <pierrysavio.pi@gmail.com>
2026-01-07 09:40:29 -0300

    chore: add write, keyPressRelease and needsShift methods implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,21 @@
 package application;
 
+import java.awt.AWTException;
+
 import util.StringWriter;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		StringWriter.write("pudim", 4, 1);
-
+		//! WARNING !
+		// NOT SUPPORTED CHARACTERS --> "!@#$%^&*()_+{}|:\"<>?"
+		
+		StringWriter sw = new StringWriter();
+		try {
+			sw.write("!@#$%^&*()_+{}|:\\\"<>", 1, 0);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/util/StringWriter.java
+++ src/util/StringWriter.java
@@ -1,18 +1,46 @@
 package util;
 
+import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
 public final class StringWriter {
 	
 	public StringWriter() {};
 	
-	public static void write(String txt, int initialDelayInSeconds, int delay) {
-		try {
-			Robot robot = new Robot();
-			robot.setAutoDelay(delay);
-			robot.delay(initialDelayInSeconds*1000);
-		} catch (Exception e) {
-			e.printStackTrace();
+	public void write(String txt, int initialDelayInSeconds, int delay) throws AWTException {
+		
+		Robot robot = new Robot();
+		robot.setAutoDelay(delay);
+		robot.delay(initialDelayInSeconds*1000);
+			
+		for (int i = 0; i<txt.length(); i++) {
+			char letter = txt.charAt(i);
+			keyPressRelease(letter, robot);
 		}
+
+	}
+	
+	private void keyPressRelease(char letter, Robot robot) throws AWTException {
+		
+		int key = KeyEvent.getExtendedKeyCodeForChar(letter);
+			
+		if (key != KeyEvent.VK_UNDEFINED) {
+			
+			if (needsShift(letter)) {
+				robot.keyPress(KeyEvent.VK_SHIFT);
+			}
+				
+			robot.keyPress(key);
+			robot.keyRelease(key);
+				
+			if (needsShift(letter)) {
+				robot.keyRelease(KeyEvent.VK_SHIFT);
+			}
+		}
+	}
+	
+	public boolean needsShift(char c) {
+		return Character.isUpperCase(c) || "!@#$%^&*()_+{}|:\"<>?".indexOf(c) >= 0;
 	}
 }
